0c3b72633e12ac01d0615c9eca8744e6d3f410bc
Taufik Nugraha <taufik.adinugraha@gmail.com>
2023-11-06 14:40:20 +0700

    send SMS response via API Gateway

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import json
+import requests
 import numpy as np
 from fastapi import FastAPI, Form
 
 app = FastAPI()
 
+url_send_sms = "https://api.nusasms.com/api/v3/sendsms/plain"
 
 # Endpoint to read the "inbox.txt" file
 @app.get("/read")
@@ -37,19 +39,21 @@ async def receive_sms(
         json.dump(data, json_file)
         json_file.write('\n')  # Add a newline to separate the JSON objects
 
-    try:
-        # Split message
-        info = msg.split('#')
-        event = info[1]
+    # Split message
+    info = msg.lower().split('#')
+    event = info[1]
 
-        number_candidates = {
-            'pilpres': 2,
-        }
+    number_candidates = {
+        'pilpres': 2,
+    }
 
-        format = 'kk#event#' + '#'.join([f'0{i+1}' for i in range(number_candidates[event])]) + '#invalid'
-        template_error_msg = 'cek & kirim kembali dgn format:\n' + format
+    # Check Error Type 1 (prefix)
+    if info[0] == 'kk':
 
-        # Check Error Type 1 (data completeness)
+        format = 'kk#event#' + '#'.join([f'0{i+1}' for i in range(number_candidates[event])]) + '#rusak'
+        template_error_msg = 'cek & kirim ulang dgn format:\n' + format
+
+        # Check Error Type 2 (data completeness)
         if len(info) != number_candidates[event] + 3:
             message = 'Data tidak sesuai, ' + template_error_msg
         else:
@@ -57,11 +61,11 @@ async def receive_sms(
             invalid = info[-1]
             total_votes = np.array(votes).astype(int).sum()
             summary = f'event:{event}\n' + '\n'.join([f'0{i+1}:{votes[i]}' for i in range(number_candidates[event])]) + f'\ninvalid:{invalid}' + f'\ntotal:{total_votes}\n'
-            # Check Error Type 2 (maximum votes is 300)
+            # Check Error Type 3 (maximum votes is 300)
             if total_votes > 300:
                 message = summary + 'Jumlah suara melebihi 300, ' + template_error_msg
             else:
-                message = summary + 'Data berhasil diterima. Jika ada koreksi, kirim kembali dgn format yg sama:\n' + format
+                message = summary + 'Berhasil diterima. Utk koreksi, kirim ulang dgn format yg sama:\n' + format
 
                 # Forward data to Bubble API
                 output = {
@@ -74,8 +78,12 @@ async def receive_sms(
                     'smsTotal': total_votes
                 }
 
-        # Return the message as the API response
-        return {"message": message}
-    
-    except:
-        pass
+        # Return the message to the sender via SMS Gateway
+        params = {
+            "user": "taufikadinugraha_api",
+            "password": "SekarangSeriusSMS@ku99",
+            "SMSText": message,
+            "GSM": originator,
+            "output": "json",
+        }
+        requests.get(url_send_sms, params=params)
