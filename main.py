5505be70cac3402c6f93139a8fa72e78bab68f9c
Taufik Nugraha <taufik.adinugraha@gmail.com>
2023-11-28 11:03:32 +0700

    fix datetime with timezone

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ for port in range(1, num_endpoints + 1):
                             status = 'SMS Only'
 
                         # Convert receive_date_str to datetime format
-                        tmp = datetime.strptime(receive_date, "%Y-%m-%d %H:%M:%S")
+                        tmp = datetime.strptime(receive_date, "%Y-%m-%d %H:%M:%S %z")
                         # Extract the hour as an integer
                         hour = tmp.hour
 
