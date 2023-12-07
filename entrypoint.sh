14b37ce5347a4176339233bb6b021d111b780f67
Taufik Nugraha <taufik.adinugraha@gmail.com>
2023-12-08 06:43:18 +0700

    fix docker logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python scto_updater.py 2>&1 &
-exec uvicorn main:app --host 0.0.0.0 --port 8008
+python scto_updater.py > /app/logs/scto_updater.log 2>&1 &
+exec uvicorn main:app --host 0.0.0.0 --port 8008 2>&1 | tee -a /app/logs/uvicorn.log
