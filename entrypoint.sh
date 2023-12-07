8a2521ecc9fc2b316c58417a91c371508b953b12
Taufik Nugraha <taufik.adinugraha@gmail.com>
2023-12-08 06:56:36 +0700

    fix logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Create log directories
+mkdir -p /app/logs
+
+# Start SCTO updater in background and redirect output to a log file
 python scto_updater.py > /app/logs/scto_updater.log 2>&1 &
+
+# Start uvicorn and append output to another log file
 exec uvicorn main:app --host 0.0.0.0 --port 8008 2>&1 | tee -a /app/logs/uvicorn.log
